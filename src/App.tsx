4a2376788efbc3396a34617716517f845517f3d6
hayoung123 <proshy1@naver.com>
2022-10-23 22:32:49 +0900

    feat: 필요한 라이브러리 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <h1 className='text-3xl font-bold'>Hello world!</h1>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <h1 className='text-3xl font-bold'>Hello world!</h1>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
---
+++ src/page/MainPage.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+interface Props {}
+
+const MainPage: FC<Props> = () => {
+  return <div></div>;
+};
+
+export default MainPage;
